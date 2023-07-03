644788ad91a712f37ca23b7d15618b943b3a7d57
jfmarkan <jfmarkan@gmail.com>
2023-07-03 16:23:38 +0200

    Adding icons to parking and vote info

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div class="container">
@@ -53,7 +54,7 @@
                 <tr>
                     <th scope="col">Name</th>
                     <th scope="col">Description</th>
-                    <th scope="col">Parking</th>
+                    <th scope="col text-center">Parking</th>
                     <th scope="col">Vote</th>
                     <th scope="col">Downtown distance</th>
                 </tr>
@@ -61,11 +62,25 @@
             <tbody>
                 <?php foreach ($hotels as $singleHotel) { ?>
                     <tr>
-                        <th scope="row"><?php echo $singleHotel['name']; ?></th>
-                        <td><?php echo $singleHotel['description']; ?></td>
-                        <td><?php echo $singleHotel['parking']; ?></td>
-                        <td><?php echo $singleHotel['vote']; ?></td>
-                        <td><?php echo $singleHotel['distance_to_center']; ?></td>
+                        <th scope="row">
+                            <?php echo $singleHotel['name']; ?>
+                        </th>
+                        <td>
+                            <?php echo $singleHotel['description']; ?>
+                        </td>
+                        <td class="text-center">
+                            <?php if ($singleHotel['parking'] === true) {
+                                echo '<i class="fa-solid fa-check"></i>';
+                            } else {
+                                echo '<i class="fa-solid fa-xmark"></i>';
+                            } ?>
+                        </td>
+                        <td class="text-center">
+                            <?php echo $singleHotel['vote']; ?>
+                        </td>
+                        <td class="text-center">
+                            <?php echo $singleHotel['distance_to_center'] .' km'; ?>
+                        </td>
                     </tr>
                 <?php } ?>
             </tbody>
